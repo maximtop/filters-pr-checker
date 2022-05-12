bea6b609ceb0f7f6f56a59f41049fae18c5f0384
Maxim Topciu <mtopciu@adguard.com>
2022-05-12 10:12:00 +0300

    remove all

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import 'dotenv/config';
+// import 'dotenv/config';
 
 import * as core from '@actions/core';
 // import fs from 'fs-extra';
@@ -20,104 +20,100 @@ import { chromium } from 'playwright-chromium';
  * - append screenshots in comments
  */
 const run = async () => {
+    // // const owner = 'maximtop';
+    // // const repo = 'AdguardFilters';
+    // // // FIXME get current pr
+    // // const pullNumber = 1;
+    // //
+    // // const prInfo = await api.getPullRequest({
+    // //     owner,
+    // //     repo,
+    // //     pullNumber,
+    // // });
+    // //
+    // // if (!prInfo.body) {
+    // //     throw new Error('Pull request description is required');
+    // // }
+    // //
+    // // const pullRequestFiles = await api.getPullRequestFiles({
+    // //     owner,
+    // //     repo,
+    // //     pullNumber,
+    // // });
+    // //
+    // // const baseFileContent = await api.getContent({
+    // //     owner: prInfo.base.owner,
+    // //     repo: prInfo.base.repo,
+    // //     path: pullRequestFiles[0],
+    // //     ref: prInfo.base.sha,
+    // // });
+    // //
+    // // const headFileContent = await api.getContent({
+    // //     owner: prInfo.head.owner,
+    // //     repo: prInfo.head.repo,
+    // //     path: pullRequestFiles[0],
+    // //     ref: prInfo.head.sha,
+    // // });
+    // //
+    // // await fs.writeFile('head.txt', headFileContent);
+    // // await fs.writeFile('base.txt', baseFileContent);
+    //
+    // const url = getUrlFromDescription(prInfo.body);
+    //
+    // if (!url) {
+    //     throw new Error('URL in the pull request is required');
+    // }
+
+    const pathToExtension = path.join(__dirname, './extension');
+    console.log(pathToExtension);
+
+    const userDataDir = '/tmp/test-user-data-dir';
+    const browserContext = await chromium.launchPersistentContext(userDataDir, {
+        headless: false,
+        args: [
+            `--disable-extensions-except=${pathToExtension}`,
+            `--load-extension=${pathToExtension}`,
+        ],
+    });
+
     try {
-        // // const owner = 'maximtop';
-        // // const repo = 'AdguardFilters';
-        // // // FIXME get current pr
-        // // const pullNumber = 1;
-        // //
-        // // const prInfo = await api.getPullRequest({
-        // //     owner,
-        // //     repo,
-        // //     pullNumber,
-        // // });
-        // //
-        // // if (!prInfo.body) {
-        // //     throw new Error('Pull request description is required');
-        // // }
-        // //
-        // // const pullRequestFiles = await api.getPullRequestFiles({
-        // //     owner,
-        // //     repo,
-        // //     pullNumber,
-        // // });
-        // //
-        // // const baseFileContent = await api.getContent({
-        // //     owner: prInfo.base.owner,
-        // //     repo: prInfo.base.repo,
-        // //     path: pullRequestFiles[0],
-        // //     ref: prInfo.base.sha,
-        // // });
-        // //
-        // // const headFileContent = await api.getContent({
-        // //     owner: prInfo.head.owner,
-        // //     repo: prInfo.head.repo,
-        // //     path: pullRequestFiles[0],
-        // //     ref: prInfo.head.sha,
-        // // });
-        // //
-        // // await fs.writeFile('head.txt', headFileContent);
-        // // await fs.writeFile('base.txt', baseFileContent);
-        //
-        // const url = getUrlFromDescription(prInfo.body);
-        //
-        // if (!url) {
-        //     throw new Error('URL in the pull request is required');
-        // }
-
-        const pathToExtension = path.join(__dirname, './extension');
-        console.log(pathToExtension);
-
-        const userDataDir = '/tmp/test-user-data-dir';
-        const browserContext = await chromium.launchPersistentContext(userDataDir, {
-            headless: false,
-            args: [
-                `--disable-extensions-except=${pathToExtension}`,
-                `--load-extension=${pathToExtension}`,
-            ],
-        });
-
-        try {
-            const backgroundPages = browserContext.backgroundPages();
-            const backgroundPage = backgroundPages.length
-                ? backgroundPages[0]
-                : await browserContext.waitForEvent('backgroundpage');
-            console.log(backgroundPage);
-        } catch (e) {
-            console.log(e.message);
-        }
-
-        // const result = await backgroundPage.evaluate(() => window.playwright);
-        // assert(result === 'test');
-        // console.log('Found expected value');
-
-        const page = await browserContext.newPage();
-        await page.goto('https:///example.org');
-        const h1 = await page.locator('h1');
-        console.log(await h1.textContent());
-
-        // Test the background page as you would any other page.
-        await browserContext.close();
-
-        // const browserContext = await chromium.launchPersistentContext('/tmp/user-data-dir');
-
-        // const context = await extension.start();
-        // await extension.config(context, headFileContent.toString());
-        // await screenshot(context, { url, path: 'image.jpeg' });
-        // await extension.config(context, 'example.org##h1');
-        // await screenshot(context, { url: 'https://example.org', path: 'image.jpeg' });
-
-        // await extension.config(context, baseFileContent.toString());
-        // await screenshot(context, { url, path: 'image2.jpeg' });
-
-        // TODO unite in one module
-        // await context.browserContext.close();
-
-        // eslint-disable-next-line no-console
-        // console.log(JSON.stringify(pullRequest));
+        const backgroundPages = browserContext.backgroundPages();
+        const backgroundPage = backgroundPages.length
+            ? backgroundPages[0]
+            : await browserContext.waitForEvent('backgroundpage');
+        console.log(backgroundPage);
     } catch (e) {
-        core.setFailed(e.message);
+        console.log(e.message);
     }
+
+    // const result = await backgroundPage.evaluate(() => window.playwright);
+    // assert(result === 'test');
+    // console.log('Found expected value');
+
+    const page = await browserContext.newPage();
+    await page.goto('https:///example.org');
+    const h1 = await page.locator('h1');
+    console.log(await h1.textContent());
+
+    // Test the background page as you would any other page.
+    await browserContext.close();
+
+    // const browserContext = await chromium.launchPersistentContext('/tmp/user-data-dir');
+
+    // const context = await extension.start();
+    // await extension.config(context, headFileContent.toString());
+    // await screenshot(context, { url, path: 'image.jpeg' });
+    // await extension.config(context, 'example.org##h1');
+    // await screenshot(context, { url: 'https://example.org', path: 'image.jpeg' });
+
+    // await extension.config(context, baseFileContent.toString());
+    // await screenshot(context, { url, path: 'image2.jpeg' });
+
+    // TODO unite in one module
+    // await context.browserContext.close();
+
+    // eslint-disable-next-line no-console
+    // console.log(JSON.stringify(pullRequest));
 };
 
 const main = async () => {
