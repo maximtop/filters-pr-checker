628742b7ed97f5214d37e95289677e079cec50ac
Maxim Topciu <mtopciu@adguard.com>
2022-05-13 08:07:07 +0300

    use action for pr only

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 
 import * as core from '@actions/core';
+import * as gh from '@actions/github';
 import { github, imgur } from './api';
 import { getUrlFromDescription } from './helpers';
 import { screenshot } from './screenshot';
@@ -14,71 +15,73 @@ import { extension } from './extension';
  * - append screenshots in comments
  */
 const run = async () => {
+    // FIXME get current pr and repo
     const owner = 'maximtop';
     const repo = 'AdguardFilters';
-    // FIXME get current pr
     const pullNumber = 1;
 
-    const prInfo = await github.getPullRequest({
-        owner,
-        repo,
-        pullNumber,
-    });
+    console.log(gh.context);
 
-    if (!prInfo.body) {
-        throw new Error('Pull request description is required');
-    }
-
-    const pullRequestFiles = await github.getPullRequestFiles({
-        owner,
-        repo,
-        pullNumber,
-    });
-
-    const baseFileContent = await github.getContent({
-        owner: prInfo.base.owner,
-        repo: prInfo.base.repo,
-        path: pullRequestFiles[0],
-        ref: prInfo.base.sha,
-    });
-
-    const headFileContent = await github.getContent({
-        owner: prInfo.head.owner,
-        repo: prInfo.head.repo,
-        path: pullRequestFiles[0],
-        ref: prInfo.head.sha,
-    });
-
-    const url = getUrlFromDescription(prInfo.body);
-
-    if (!url) {
-        throw new Error('URL in the pull request is required');
-    }
-
-    const context = await extension.start();
-
-    await extension.config(context, baseFileContent.toString());
-    const baseScreenshot = await screenshot(context, { url, path: 'base_image.jpeg' });
-
-    await extension.config(context, headFileContent.toString());
-    const headScreenshot = await screenshot(context, { url, path: 'head_image.jpeg' });
-
-    // TODO unite in one module
-    await context.browserContext.close();
-
-    const [baseLink, headLink] = await Promise.all([
-        imgur.upload(baseScreenshot),
-        imgur.upload(headScreenshot),
-    ]);
-
-    const body = `before: ![baseScreenshot](${baseLink}) \r\nafter:![headScreenshot](${headLink})`;
-
-    await github.createComment({
-        repo,
-        owner,
-        issueNumber: pullNumber,
-        body,
-    });
+    // const prInfo = await github.getPullRequest({
+    //     owner,
+    //     repo,
+    //     pullNumber,
+    // });
+    //
+    // if (!prInfo.body) {
+    //     throw new Error('Pull request description is required');
+    // }
+    //
+    // const pullRequestFiles = await github.getPullRequestFiles({
+    //     owner,
+    //     repo,
+    //     pullNumber,
+    // });
+    //
+    // const baseFileContent = await github.getContent({
+    //     owner: prInfo.base.owner,
+    //     repo: prInfo.base.repo,
+    //     path: pullRequestFiles[0],
+    //     ref: prInfo.base.sha,
+    // });
+    //
+    // const headFileContent = await github.getContent({
+    //     owner: prInfo.head.owner,
+    //     repo: prInfo.head.repo,
+    //     path: pullRequestFiles[0],
+    //     ref: prInfo.head.sha,
+    // });
+    //
+    // const url = getUrlFromDescription(prInfo.body);
+    //
+    // if (!url) {
+    //     throw new Error('URL in the pull request is required');
+    // }
+    //
+    // const context = await extension.start();
+    //
+    // await extension.config(context, baseFileContent.toString());
+    // const baseScreenshot = await screenshot(context, { url, path: 'base_image.jpeg' });
+    //
+    // await extension.config(context, headFileContent.toString());
+    // const headScreenshot = await screenshot(context, { url, path: 'head_image.jpeg' });
+    //
+    // // TODO unite in one module
+    // await context.browserContext.close();
+    //
+    // const [baseLink, headLink] = await Promise.all([
+    //     imgur.upload(baseScreenshot),
+    //     imgur.upload(headScreenshot),
+    // ]);
+    //
+    // const body = `before: ![baseScreenshot](${baseLink}) \r\nafter:![headScreenshot](${headLink})`;
+    //
+    // await github.createComment({
+    //     repo,
+    //     owner,
+    //     issueNumber: pullNumber,
+    //     body,
+    // });
 };
 
 const main = async () => {
